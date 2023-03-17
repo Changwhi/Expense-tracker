40dc2fd9ae7964f3ef9b43ce86792573a498ceef
Changwhi <whinada@gmail.com>
2023-03-17 02:48:51 -0700

    use javascript property

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
+import { wait } from '@testing-library/user-event/dist/utils';
 import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate = new Date(2001, 2, 22);
+    const expenseTitle = "Title";
+    const expnesePrice = 44.44;
+
+    console.log(expenseDate.toISOString());
     return (
         <div className='expense-item'>
-        <div>Date</div>
-        <div className='expense-item__descrption'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expnesePrice}</div>
         </div>
         </div>
 );
